6df0dff714f8c59ff48258a6eda07f0473bbe24f
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-12 21:52:36 -0500

    add: agregar libreria colors

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Este proyecto es el backend de una aplicación web para administración de tarea
 # Avances del Proyecto
 
 1.- Configuración inicial del proyecto
+
+
+# Comandos utilizados:
+
+npm init --y
+npm i express
+npm i -D @types/express
+npm i -D nodemo
+npm i colors
+
+
+npm run dev
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from 'colors';
 import server from './server';
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(colors.bgGreen.bold(`Server is running on http://localhost:${port}`));
 });
