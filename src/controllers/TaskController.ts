9264d94b855204093f635dd928b18769f9f27521
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-17 21:57:00 -0500

    add: crear middleware que valide si un proyecto existe

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -4,17 +4,12 @@ import Task from "../models/Task"
 
 export class TaskController {
     static createTask = async (req: Request, res: Response) => {
-        const { projectId } = req.params
-        const project = await Project.findById(projectId)
-        if (!project) {
-            return res.status(404).json({ message: "Proyecto no encontrado!" })
-        }
         try {
             const task = new Task(req.body)
-            task.project = project.id
-            project.tasks.push(task.id)
+            task.project = req.project.id
+            req.project.tasks.push(task.id)
             await task.save()
-            await project.save()
+            await req.project.save()
             res.send('Tarea Creada Correctamente')
         } catch (error) {
             console.error(error)
