7997cdd28d224e2256fb078e2b743105caadf018
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-10-21 21:50:50 -0500

    add: ajustes endpoint para remover miembro de un equipo

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -37,13 +37,13 @@ export class TeamMemberController {
     }
 
     static removeMemberById = async (req: Request, res: Response) => {
-        const { id } = req.body
+        const { userId } = req.params
 
-        if (!req.project.team.some(team => team.toString() === id)) {
+        if (!req.project.team.some(team => team.toString() === userId)) {
             return res.status(404).json({ error: 'Usuario no encontrado en el equipo' })
         }
 
-        req.project.team = req.project.team.filter(teamMember => teamMember.toString() !== id)
+        req.project.team = req.project.team.filter(teamMember => teamMember.toString() !== userId)
 
         await req.project.save()
 
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -98,8 +98,8 @@ router.post('/:projectId/team',
     handleInputErrors,
     TeamMemberController.addMemberById
 )
-router.delete('/:projectId/team',
-    body('id')
+router.delete('/:projectId/team/:userId',
+    param('userId')
         .isMongoId().withMessage('Id no válido'),
     handleInputErrors,
     TeamMemberController.removeMemberById
